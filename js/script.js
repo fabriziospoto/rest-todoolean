f1238435a06918954f33a03cc121b04bd0f46c0d
Fabrizio <fabriziospoto1@gmail.com>
2020-09-09 17:46:15 +0200

    Parziale 3
    Parziale 3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
+
     $('#aggiungi').click(function(){
         $('.lista').empty();
-        var inputUtente = $('input').val();
+        var inputUtente = $('#user').val();
         aggiungiInput(inputUtente);
         richiama();
     });
@@ -9,7 +10,7 @@ $( document ).ready(function() {
     $(document).keydown(function(e) {
        if (e.keycode == 13 || e.which == 13) {
            $('.lista').empty();
-           var inputUtente = $('input').val();
+           var inputUtente = $('#user').val();
            aggiungiInput(inputUtente);
            richiama();
        }
@@ -21,6 +22,24 @@ $( document ).ready(function() {
        elimina(x);
        richiama();
    });
+
+   $(document).on('click', '.voce-lista', function(){
+       var inputModify = $(this).find('.modify');
+       var testoVoce = $(this).find('.testo');
+
+       inputModify.removeClass('hide');
+       testoVoce.addClass('hide');
+
+   });
+
+   $(document).keydown(function(e) {
+        if (e.keycode == 13 || e.which == 13) {
+            $('.lista').empty();
+            var newInput = $('#id-input').val();
+            modifica(newInput);
+            // richiama();
+        }
+    });
 });
 
 //******FUNZIONI
@@ -76,6 +95,34 @@ function elimina(id) {
             success: function(risposta) {
                 $('.lista').html(' ');
                 richiama();
+            },
+            error: function(){
+                alert('Errore');
+            }
+        }
+    )
+}
+
+function modifica(n) {
+    $.ajax(
+        {
+            url: 'http://157.230.17.132:3032/todos',
+            method:'PATCH',
+            data: {
+                text: n
+            },
+            success: function(data) {
+                var source = $("#entry-template").html();
+                var template = Handlebars.compile(source);
+                for (var i = 0; i < data.length; i++) {
+                    var context = {
+                        text: data[i].text,
+                        id: data[i].id
+                    }
+                    //console.log(context);
+                    var html = template(context);
+                    $('.lista').append(html);
+                }
             },
             error: function(){
                 alert('Errore');
